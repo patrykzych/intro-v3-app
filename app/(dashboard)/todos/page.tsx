ab1b6dce86aea4ae6b387e7d1d1d66ad4b5b6be6
patryk zych <patryk.zych@trustmate.io>
2025-04-12 15:02:16 +0200

    todos component with prisma get function

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,3 +1,15 @@
-export default function Todos() {
-  return <div>Todos</div>
+import TodoList from '@/app/components/TodoList'
+import db from '@/utils/db'
+
+const getData = async () => {
+  const todos = await db.todo.findMany({})
+  return todos
+}
+export default async function Todos() {
+  const todos = await getData()
+  return (
+    <div>
+      <TodoList todos={todos} />
+    </div>
+  )
 }
